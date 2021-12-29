59c725a21511396b8a9e46d3ceaadbc84ac3ae2d
AdamKasinski <50599406+AdamKasinski@users.noreply.github.com>
2021-12-30 00:27:55 +0100

    Update app.py

--- app.py
+++ app.py
@@ -3,5 +3,4 @@ import dash_bootstrap_components as dbc
 import dash_html_components as html
 
 app = dash.Dash(__name__, external_stylesheets=[dbc.themes.BOOTSTRAP])
-#server = app.server
-server = flask.Flask(__name__)
+server = app.server
